a8f6b4ea3a0294befe13040ca907644acca74e5c
hannahchun <80413027+hannahchun@users.noreply.github.com>
2023-02-27 18:01:01 +0900

    [C++] Convert integer to character

---
+++ String/intToChar_sol1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int seven_int = 7;
+    char seven_char = (char) (seven_int + 48);
+    cout << "Integer converted to Char : " << seven_char <<endl;
+    return 0;
+}
