b77bd766d8b7d81166f35a45d8e852dfaaaae0f1
hannahchun <80413027+hannahchun@users.noreply.github.com>
2023-02-27 17:41:45 +0900

    [C++] Convert string to integer 수정

--- String/strToInt_sol1.cpp
+++ String/strToInt_sol1.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 
 int main() {
-   string str = "7";
+   string s1 = "7";
+
+   // string class 이용 안하고 그냥 char s1[] = "7"; 하는 것도 가능!
+
    int num;
 
-   num = stoi (str);
+   num = stoi (s1);
    cout << "String converted to integer : " << num << endl;
 }
