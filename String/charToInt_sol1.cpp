d4ea0ba2ee4815715d6523bffc8bf4c370d4ea25
hannahchun <80413027+hannahchun@users.noreply.github.com>
2023-02-27 18:00:15 +0900

    [C++] Convert character to integer

---
+++ String/charToInt_sol1.cpp
@@ -0,0 +1,13 @@
+// 한 문자를 int로 바꾸는 작업 
+// ex. '7' -> 7 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char seven_char = '7';
+    int seven_int = seven_char - '0';
+    int res = seven_int + 1;
+    cout << seven_char << " + 1 is " << res << endl;
+    return 0;
+}
